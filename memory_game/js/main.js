401306fbae08f737ca3043ef989a8e5f9cfcfce8
Michael Dudley-Marquez <sound.plus.fury@gmail.com>
2018-12-21 14:58:17 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,15 +23,17 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match!");
+    alert("You found a match!");
 	} else {
-    /*console.log*/alert("Sorry, try again.");
+    alert("Sorry, try again.");
 	}
 };
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
+	console.log("User flipped " + cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].suit);
 	if (cardsInPlay.length === 2) {
@@ -39,6 +41,19 @@ var flipCard = function(cardId) {
 	}
  };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+	cardElement.setAttribute('src', 'images/back.png');
+	cardElement.setAttribute('data-id', i);
+	cardElement.addEventListener('click', flipCard)
+	document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+
+createBoard(); 
+
+
+
 
